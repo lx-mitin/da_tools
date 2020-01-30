f7cc5b0bf9a5e14f25387c3f662bfbb534440dc0
lx.mitin@gmail.com <lx.mitin@gmail.com>
2020-01-30 07:48:16 +0400

    Show frequency for top 5 values for each column.

--- df_tools/unique.py
+++ df_tools/unique.py
@@ -4,7 +4,10 @@
 def unique(df):
     """Return quantity and samples of unique values for each column."""
     uniques = dict(df.nunique())
+
     for k in uniques.keys():
-        uniques[k] = {'quantity':uniques[k], 'list':df[k].unique()}
+        vc = df[k].value_counts(normalize=True, dropna=False).head()
+        vc = vc.map(lambda v: round(v, 4))
+        uniques[k] = {'unique_counts': uniques[k], 'value_counts': dict(vc)}
 
     return uniques
